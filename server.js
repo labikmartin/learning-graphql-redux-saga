fc178b83b835c82c4845099697c0cb9d887c78d5
Labik Martin <labikm@cngroup.dk>
2019-10-16 15:39:30 +0200

    Added GraphQL and initial GQL setup

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const MyGraphQLSchema = require('./schema');
+
+const app = express();
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: MyGraphQLSchema,
+    graphiql: true
+  })
+);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT),
+  () => {
+    console.log(`Server listening on: ${PORT}`);
+  };
