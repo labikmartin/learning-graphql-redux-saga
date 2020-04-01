40dcd4bdb306819be8df8deea8ce1e595c5baa9a
Labik Martin <labikm@cngroup.dk>
2020-04-01 10:26:44 +0200

    Project setup | Server, Schema, Apollo client, Fetching client data

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const graphqlHTTP = require('express-graphql');
 const MyGraphQLSchema = require('./schema');
 
 const app = express();
+
+// Allow cors
+app.use(cors());
 
 app.use(
   '/graphql',
@@ -14,7 +18,6 @@ app.use(
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT),
-  () => {
-    console.log(`Server listening on: ${PORT}`);
-  };
+app.listen(PORT, () => {
+  console.log(`Server listening on: ${PORT}`);
+});
